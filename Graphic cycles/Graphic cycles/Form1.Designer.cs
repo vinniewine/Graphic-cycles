187433ccddaa68836489557d4916ea0d537acb4a
vlada <107725482+vinniewine@users.noreply.github.com>
2022-07-11 22:50:05 +0100

    New buttons
    1. Added button 2 to draw horizontal lines.
      2. Added button 3 to draw vertical lines.
      3. Added button 4 to draw a grid.
      4. Added the Sleep1() method for a 100 ms delay.
      5. Added a delay inside each cycle.

--- Graphic cycles/Graphic cycles/Form1.Designer.cs
+++ Graphic cycles/Graphic cycles/Form1.Designer.cs
@@ -31,15 +31,18 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.buttonClear = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.pictureBox1.Location = new System.Drawing.Point(140, 72);
+            this.pictureBox1.Location = new System.Drawing.Point(137, 27);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.Size = new System.Drawing.Size(301, 301);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -63,11 +66,44 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(471, 179);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(52, 52);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(471, 254);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(52, 52);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(471, 325);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(52, 52);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(599, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.buttonClear);
             this.Controls.Add(this.pictureBox1);
@@ -75,7 +111,7 @@
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Graph Cycles";
+            this.Text = "v";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -86,6 +122,9 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button buttonClear;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- Graphic cycles/Graphic cycles/Form1.cs
+++ Graphic cycles/Graphic cycles/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -43,11 +44,55 @@ namespace Graphic_cycles
         {
             pictureBox1.Refresh();
         }
+        
+
+        public void Sleep1()
+        {
+            RefreshPicture();
+           Thread.Sleep(100);
+        }
+
+        
 
         private void button1_Click(object sender, EventArgs e)
         {
             g.DrawLine(pen, 0, 0, 300, 300);
             g.DrawLine(pen, 300, 0,0, 300);
+            RefreshPicture();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int y = 0; y <= 300; y += 30)
+            {
+
+                g.DrawLine(pen, 0, y, 300, y);
+                Sleep1();
+            }
+            RefreshPicture();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (int x = 0; x <= 300; x += 30)
+            {
+                g.DrawLine(pen, x, 0, x, 300);
+                Sleep1();
+                    }
+          
+            RefreshPicture();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            for (int y = 0; y <= 300; y += 30)
+            {
+                g.DrawLine(pen, 0, y, 300, y);
+                Sleep1();
+                int x = y;
+                g.DrawLine(pen, x, 0, x, 300);
+                Sleep1();
+            }
             RefreshPicture();
         }
     }
